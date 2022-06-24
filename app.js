34534dec7bb562134dafc953da64f52091392b44
Olasunkanmi56 <olasunkanmiolamide46@gmail.com>
2022-06-25 00:46:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const dname = prompt('what is your name');
+const dheight = prompt('what is your height');
+const dcountry = prompt('what is your country');
+
+     console.log(`NAME:${dname}`);
+     console.log(`HEIGHT:${dheight}m`);
+     console.log(`COUNTRY:${dcountry}}`);
